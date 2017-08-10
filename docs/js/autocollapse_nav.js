07771d1ba310dc09b7cd43021c46f90058da5ed7
Marcus Fedarko <marcus.fedarko@gmail.com>
2017-08-10 03:36:43 -0400

    Add comments re: info site JS

--- docs/js/autocollapse_nav.js
+++ docs/js/autocollapse_nav.js
@@ -1,3 +1,8 @@
+/* Once the user clicks on a link within the nav (when the nav is in its
+ * "compressed" view, e.g. for small screens), this automatically collapses the
+ * nav. This makes the user experience of going somewhere on the page feel a
+ * lot more fluid, since they don't have to manually close the nav.
+ */
 $(function() {
     $("nav a").on("click", function() {
         if ($("#main-navbar-content").hasClass("in")) {
