e51f1b3d9f86e85ffb55ef87a8de9e152e1fb0cc
Marcus Fedarko <mfedarko@umd.edu>
2018-03-15 03:58:36 -0400

    Add colorpicker, etc. for "current path" color

--- viewer/js/xdot2cy.js
+++ viewer/js/xdot2cy.js
@@ -108,7 +108,7 @@ var DEFAULT_NODE_COLOR = undefined;
 // (this can be done via the commands :%s/\n/\\n/g and :%s/\t/\\t/g in Vim).
 // Ideally this process would be automated, but there have been some issues
 // with that (see issue #263 on GitHub for a bit of a summary).
-var DEFAULT_COLOR_SETTINGS = "mincncp\t#0022ff\nmaxcncp\t#ff2200\ncnlcp\t#ffffff\ncsnlcp\t#ffffff\nusncp\t#888888\nbubblecp\t#9abaf3\nfropecp\t#59f459\nchaincp\t#fcaca3\nychaincp\t#ffd163\nspqrscp\t#ffd644\nspqrpcp\t#eb8ef9\nspqrrcp\t#31bf6f\nbicmpcp\t#e9e9e9\ntnbcp\t#000000\ntngbcp\t#000000\nmiscpatterncp\t#c398eb\nusnlcp\t#ffffff\nsnlcp\t#ffffff\nusecp\t#555555\nsecp\t#111111\nhoecp\t#ff0000\nhosecp\t#800000\nloecp\t#0000ff\nlosecp\t#000080\ncngcccp\t#000000\nsngbcp\t#000000\nbgcp\t#ffffff\n";
+var DEFAULT_COLOR_SETTINGS = "mincncp\t#0022ff\nmaxcncp\t#ff2200\ncnlcp\t#ffffff\ncsnlcp\t#ffffff\nusncp\t#888888\nbubblecp\t#9abaf3\nfropecp\t#59f459\nchaincp\t#fcaca3\nychaincp\t#ffd163\nspqrscp\t#ffd644\nspqrpcp\t#eb8ef9\nspqrrcp\t#31bf6f\nbicmpcp\t#e9e9e9\ntnbcp\t#000000\ntngbcp\t#000000\nmiscpatterncp\t#c398eb\nusnlcp\t#ffffff\nsnlcp\t#ffffff\nusecp\t#555555\nsecp\t#111111\nhoecp\t#ff0000\nhosecp\t#800000\nloecp\t#0000ff\nlosecp\t#000080\ncngcccp\t#000000\nsngbcp\t#000000\ncpcp\t#994700\nbgcp\t#ffffff\n";
 // The background color of the graph. Set in initGraph().
 var BG_COLOR = undefined;
 // Booleans for whether or not to use certain performance options
@@ -514,7 +514,7 @@ function initGraph(viewType) {
             {
                 selector: 'node.currpath',
                 style: {
-                    'background-color': '#994700',
+                    'background-color': $("#cpcp").colorpicker("getValue"),
                 }
             },
             {
