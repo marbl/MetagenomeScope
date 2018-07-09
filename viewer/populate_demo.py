12229da082e9e1f9f18c0cf72ba1fdf8341f124e
Marcus Fedarko <mfedarko@umd.edu>
2018-07-08 23:05:03 -0400

    Add cmdline script: populates V.I. demo .db dialog
    Pretty satisfied with how this turned out! Should make generating
    custom demos a lot easier in the future.

---
+++ viewer/populate_demo.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# Copyright (C) 2017-2018 Marcus Fedarko, Jay Ghurye, Todd Treangen, Mihai Pop
+# Authored by Marcus Fedarko
+#
+# This file is part of MetagenomeScope.
+# 
+# MetagenomeScope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MetagenomeScope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MetagenomeScope.  If not, see <http://www.gnu.org/licenses/>.
+####
+# Generates a copy of the viewer interface index.html page where the demo files
+# available to select in the "Demo .db" dialog match those in a specified list.
+# Right now the input list is a tab-separated values (TSV) file, where each
+# line is "filename\tdescription".
+#
+# (Descriptions can include HTML, e.g. for hyperlinks to the data source.)
+# Possible TODO: Add description parameter to db's? Would allow this sort of
+# thing to work without extra input/etc. -- the user could just pass a
+# directory, and this script could iterate through all db's in that directory
+# and autogenerate an index.html file.
+
+import argparse
+
+DB_HTML_MARGIN = "                       " 
+DB_HTML_TEMPLATE = """
+                        <div class="radio">
+                            <label>
+                                <input type="radio" name="fs" id="{ID}"{CHECKED}>
+                                {DESC}
+                            </label>
+                        </div>
+                        """
+CHECKED = "\n" + DB_HTML_MARGIN + "checked=\"checked\""
+DB_LIST_START_TAG = "<!-- BEGIN DEMO .DB LIST -->"
+DB_LIST_END_TAG = "<!-- END DEMO .DB LIST -->"
+
+
+parser = argparse.ArgumentParser(description="Generates a copy of the " + \
+        "MetagenomeScope index.html page with a specified demo .db list.")
+parser.add_argument("-i", "--inputfile", required=True,
+        help="demo .db list file")
+parser.add_argument("-v", "--indexfile", required=True,
+        help="viewer interface index.html file to use as a base (this file will not be modified)")
+parser.add_argument("-o", "--outputfile", required=True,
+        help="output index.html file name")
+args = parser.parse_args()
+
+db_ct = 0
+html_output = ""
+with open(args.inputfile, "r") as db_list_file:
+    for line in db_list_file:
+        if "\t" in line:
+            db_fn, db_desc = line.split("\t")
+            # Fill in appropriate information in DB_HTML_TEMPLATE
+            db_html_output = DB_HTML_TEMPLATE.replace("{ID}", db_fn)
+            db_html_output = db_html_output.replace("{DESC}", db_desc.strip())
+            if db_ct == 0:
+                db_html_output = db_html_output.replace("{CHECKED}", CHECKED)
+            else:
+                db_html_output = db_html_output.replace("{CHECKED}", "")
+            html_output += db_html_output
+            db_ct += 1
+
+# We've got the HTML corresponding to the demo .db list (html_output) ready.
+# Now we just need to insert it into index.html in the right place.
+# NOTE there's almost certainly a more efficient way to write this without
+# checking every line in the file for the BEGIN DEMO .DB LIST comment, but
+# considering the size of the index.html file this shouldn't be that slow
+with open(args.outputfile, "w") as output_index_file:
+    with open(args.indexfile, "r") as template_index_file:
+        going_through_template_demo_list = False
+        for line in template_index_file:
+            if not going_through_template_demo_list:
+                if DB_LIST_START_TAG in line:
+                    output_index_file.write(DB_HTML_MARGIN + DB_LIST_START_TAG)
+                    output_index_file.write(html_output)
+                    going_through_template_demo_list = True
+                else:
+                    output_index_file.write(line)
+            else:
+                if DB_LIST_END_TAG in line:
+                    output_index_file.write(DB_LIST_END_TAG + "\n")
+                    going_through_template_demo_list = False
+print "Demo .db list containing %d files inserted into output file %s." % \
+        (db_ct, args.outputfile)
