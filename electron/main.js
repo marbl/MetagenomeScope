2b38a57d33865d4c7dd2984dd485e839f10b25cf
Marcus Fedarko <mfedarko@umd.edu>
2018-08-20 23:13:23 -0400

    Initial commit of MgSc viewer Electron app #5

---
+++ electron/main.js
@@ -0,0 +1,9 @@
+// CODELINK: https://electronjs.org/docs/tutorial/first-app
+const {app, BrowserWindow} = require('electron');
+
+function createWindow() {
+    win = new BrowserWindow({backgroundColor: '#fff', width: 1200, height: 900,
+        autoHideMenuBar: true, icon: 'bubble_fancy.png'});
+    win.loadFile('index.html');
+}
+app.on('ready', createWindow);
