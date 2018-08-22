16412b5d4103cf996e927d64f834a9841df897fb
Marcus Fedarko <mfedarko@umd.edu>
2018-08-22 00:22:03 -0400

    Add longer comment header for electron main.js

--- electron/main.js
+++ electron/main.js
@@ -1,4 +1,24 @@
-// CODELINK: https://electronjs.org/docs/tutorial/first-app
+/* Copyright (C) 2017-2018 Marcus Fedarko, Jay Ghurye, Todd Treangen, Mihai Pop
+ * Authored by Marcus Fedarko
+ *
+ * This file is part of MetagenomeScope.
+ *
+ * MetagenomeScope is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MetagenomeScope is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MetagenomeScope.  If not, see <http://www.gnu.org/licenses/>.
+ ****
+ * CODELINK: The bulk of this file's code is based on the tutorial located at
+ * https://electronjs.org/docs/tutorial/first-app.
+ */
 const {app, BrowserWindow} = require('electron');
 
 function createWindow() {
