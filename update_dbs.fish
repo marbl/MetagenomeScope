e080938fe0ebb5465ecb81b68e669984f2f7985b
Marcus Fedarko <mfedarko@umd.edu>
2018-09-10 14:01:46 -0700

    Update demo graph generation script
    -Reorder the post-clean HMP dataset (august1) to be higher up in the
     order, so it gets generated earlier.
    -Comment out the pre-clean HMP dataset's generation invocation, since
     it's super slow. Once we fix the duplicate edges thing (#75), it
     should be possible to re-add this back into the script.
    -Add the -spqr flag to the Marygold paper graph, since that one we
     definitely want to have SPQR data for (I'm pretty sure this version
     of the demo graph generation script predates the -spqr option in the
     preprocessing script, which is why none of the invocations here had
     -spqr flags).

--- update_dbs.fish
+++ update_dbs.fish
@@ -12,16 +12,16 @@
 ./graph_collator/collate.py -i testgraphs/ecoli/E_coli_LastGraph -o ecoli -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/sjackman/sample.gfa -o sample_gfa -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/sjackman/loop.gfa -o loop_gfa -d testdbs/ -w
-./graph_collator/collate.py -i testgraphs/marygold/marygold_test.gml -o marygoldtest -d testdbs/ -w
+./graph_collator/collate.py -i testgraphs/marygold/marygold_test.gml -o marygoldtest -d testdbs/ -w -spqr
 ./graph_collator/collate.py -i testgraphs/sample_LastGraph -o sample_LastGraph -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/longtest_LastGraph -o longtest -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/small_ecoli/oriented_lengthinfo.gml -o small_ecoli -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/UpdatedBAMBUSfiles/shakya_oriented.gml -o shakya_new -d testdbs/ -w
+./graph_collator/collate.py -i testgraphs/august1.gml -o august1 -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/ship_hull_biofilm/oriented_judson.gml -o biofilm_judson -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/ship_hull_biofilm/oriented_judson_2.gml -o biofilm_judson2 -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/UpdatedBAMBUSfiles/oriented.gml -o 20170220 -d testdbs/ -w
-./graph_collator/collate.py -i testgraphs/august1.gml -o august1 -d testdbs/ -w
 echo "Creating the (unsimplified) Shakya graph. This might take a while."
 ./graph_collator/collate.py -i testgraphs/RF_oriented_lengthinfo.gml -o shakya -d testdbs/ -w
-echo "Creating the (unsimplified) SRS049950 graph. This might take a while."
-./graph_collator/collate.py -i testgraphs/august1_before_withlengthandfwdorients.gml -o august1_before -d testdbs/ -w
+#echo "Creating the (unsimplified) SRS049950 graph. This might take a while."
+#./graph_collator/collate.py -i testgraphs/august1_before_withlengthandfwdorients.gml -o august1_before -d testdbs/ -w
