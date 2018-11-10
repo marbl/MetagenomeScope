d9975af508dd51fe0a39e0a9f050d2aca017e665
Marcus Fedarko <mfedarko@umd.edu>
2018-11-10 01:47:19 -0800

    Make MaryGold DB creation use fixed GML: fix #134

--- update_dbs.fish
+++ update_dbs.fish
@@ -12,7 +12,7 @@
 ./graph_collator/collate.py -i testgraphs/ecoli/E_coli_LastGraph -o ecoli -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/sjackman/sample.gfa -o sample_gfa -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/sjackman/loop.gfa -o loop_gfa -d testdbs/ -w
-./graph_collator/collate.py -i testgraphs/marygold/marygold_test.gml -o marygoldtest -d testdbs/ -w -spqr
+./graph_collator/collate.py -i testgraphs/marygold_fig2a.gml -o marygoldtest -d testdbs/ -w -spqr
 ./graph_collator/collate.py -i testgraphs/sample_LastGraph -o sample_LastGraph -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/longtest_LastGraph -o longtest -d testdbs/ -w
 ./graph_collator/collate.py -i testgraphs/small_ecoli/oriented_lengthinfo.gml -o small_ecoli -d testdbs/ -w
