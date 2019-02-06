aa020a0c697958514038451bcff241105d1a9198
Marcus Fedarko <mfedarko@ucsd.edu>
2019-02-06 00:35:02 -0800

    Add test setup code (some new, some moved here)
    Forgot to commit this in in the past few minutes... it's here now :)

---
+++ viewer/tests/setup_tests.js
@@ -0,0 +1,8 @@
+mocha.setup('bdd');
+mgsc.runTests = function() {
+    mocha.run();
+    // to make a long story short: things go wrong if you try to run tests more
+    // than once without refreshing the browser, so we disable the runTests
+    // button after running tests once
+    disableButton("runTestsButton");
+}
