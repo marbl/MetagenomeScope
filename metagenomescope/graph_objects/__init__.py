e1daf871956362e03e8965d9db9b24fbcbe8713e
Marcus Fedarko <mfedarko@ucsd.edu>
2019-09-12 16:59:43 -0700

    BUG: expose SPQR objs through graph_objects pkg

--- metagenomescope/graph_objects/__init__.py
+++ metagenomescope/graph_objects/__init__.py
@@ -1,9 +1,10 @@
 from .assemblygraph import AssemblyGraph
 from .component import Component
 from .patterns import Bubble, Rope, Chain, Cycle, MiscPattern
+from .spqr_mode_objects import SPQRMetaNode, Bicomponent
 from .basic_objects import Edge, Node, NodeGroup
 
 __all__ = [
     "AssemblyGraph", "Component", "Bubble", "Rope", "Chain", "Cycle",
-    "MiscPattern", "Edge", "Node", "NodeGroup"
+    "MiscPattern", "SPQRMetaNode", "Bicomponent", "Edge", "Node", "NodeGroup"
 ]
