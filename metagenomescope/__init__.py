a30c6cf545387d41d933faa84616c4d9eb30ba56
Marcus Fedarko <mfedarko@ucsd.edu>
2019-09-13 14:52:28 -0700

    DOC: add docs to main __init__

--- metagenomescope/__init__.py
+++ metagenomescope/__init__.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2016-- Marcus Fedarko, Jay Ghurye, Todd Treangen, Mihai Pop
+# Authored by Marcus Fedarko
+#
+# This file is part of MetagenomeScope.
+#
+# MetagenomeScope is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# MetagenomeScope is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with MetagenomeScope.  If not, see <http://www.gnu.org/licenses/>.
+####
+# MetagenomeScope is a visualization tool for (meta)genomic sequence assembly
+# graphs. Once MetagenomeScope is installed, you should be able to run it from
+# the command line using the command "mgsc".
+
 from . import collate
 
 __all__ = ["collate"]
