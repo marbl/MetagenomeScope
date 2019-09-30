5da0140b88263f9bee72c3ea25fd0b45b5453fb9
Marcus Fedarko <mfedarko@umd.edu>
2019-09-29 17:49:10 -0700

    TST: add simple loop.gfa test case

--- metagenomescope/tests/assembly_graph_parser/test_parse_gfa.py
+++ metagenomescope/tests/assembly_graph_parser/test_parse_gfa.py
@@ -47,7 +47,7 @@ def check_sample_gfa_digraph(digraph):
         assert digraph.nodes[node_id]["length"] == 4
         assert digraph.nodes[node_id]["gc_content"] == 0.25
 
-    valid_edges = (
+    expected_edges = (
         ("1", "2"),
         ("-2", "-1"),
         ("3", "2"),
@@ -57,7 +57,7 @@ def check_sample_gfa_digraph(digraph):
         ("-4", "5"),
         ("-5", "4"),
     )
-    for edge_id in valid_edges:
+    for edge_id in expected_edges:
         assert edge_id in digraph.edges
 
 
@@ -71,3 +71,36 @@ def test_parse_gfa2_good():
     check_sample_gfa_digraph(
         parse_gfa("metagenomescope/tests/input/sample2.gfa")
     )
+
+def test_parse_self_implied_edge():
+    """Uses loop.gfa (c/o Shaun Jackman) to test self-implied GFA edges.
+
+    We define a "self-implied edge" as an edge whose complement is itself. An
+    example of such an edge is 2+ -> 2- in loop.gfa; the complement of this
+    edge is -(2-) -> -(2+), which is equal to 2+ -> 2- (my notation looks kind
+    of sillyt but you get the idea).
+
+    The expected behavior is that self-implied edges are only added to the
+    assembly graph visualization once, since duplicate edges in general don't
+    really make sense here. This corroborates the visualization of loop.gfa in
+    Shaun's repository containing it
+    (https://github.com/sjackman/assembly-graph).
+    """
+    digraph = parse_gfa("metagenomescope/tests/input/loop.gfa")
+    assert len(digraph.nodes) == 8
+
+    # Even though there are 4 edges specified in loop.gfa, we only expect *6*
+    # edges to be in the output graph: 2+ -> 2- and 3- -> 3+ are both
+    # self-implying, so we don't add their "complements."
+    assert len(digraph.edges) == 6
+
+    expected_edges = (
+        ("1", "1"),
+        ("-1", "-1"),
+        ("2", "-2"),
+        ("-3", "3"),
+        ("-4", "-4"),
+        ("4", "4")
+    )
+    for edge_id in expected_edges:
+        assert edge_id in digraph.edges
