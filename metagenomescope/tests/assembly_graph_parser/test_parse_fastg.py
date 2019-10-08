1f420e2d2be7a489412debc3d46953d8d737c795
Marcus Fedarko <mfedarko@ucsd.edu>
2019-10-07 19:24:34 -0700

    TST: make fastg test more detailed (from pyfastg)
    Yeah, since we're using the same test file I just copied over the test
    code. Added a codelink and everything.

--- metagenomescope/tests/assembly_graph_parser/test_parse_fastg.py
+++ metagenomescope/tests/assembly_graph_parser/test_parse_fastg.py
@@ -26,4 +26,32 @@ def get_test_fastg():
 
 def test_good():
     g = run_tempfile_test("fastg", get_test_fastg(), None, None)
+    # CODELINK: remaining code in this function is taken from
+    # test_parse_small_assembly_graph() in the pyfastg codebase.
+    # (...I mean, I literally wrote that function, but might as well cite it?)
     assert len(g.nodes) == 6
+    assert len(g.edges) == 8
+    i2length = {1: 9, 2: 3, 3: 5}
+    i2cov = {1: 4.5, 2: 100, 3: 16.5}
+    i2gc = {1: 5 / 9.0, 2: 2 / 3.0, 3: 3 / 5.0}
+    for i in range(1, 4):
+        si = str(i)
+        for suffix in ("+", "-"):
+            name = si + suffix
+            assert name in g.nodes
+            assert g.nodes[name]["cov"] == i2cov[i]
+            assert g.nodes[name]["length"] == i2length[i]
+            assert g.nodes[name]["gc"] == i2gc[i]
+
+    valid_edges = (
+        ("2+", "1+"),
+        ("2+", "3-"),
+        ("2+", "3+"),
+        ("1+", "3-"),
+        ("3-", "2-"),
+        ("3+", "1-"),
+        ("3+", "2-"),
+        ("1-", "2-"),
+    )
+    for e in valid_edges:
+        assert e in g.edges
