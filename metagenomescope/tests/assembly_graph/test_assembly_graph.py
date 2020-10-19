1f9f281db9a50081b32bacb5f3cc5b172dd4826c
Marcus Fedarko <mfedarko@umd.edu>
2020-10-19 00:43:28 -0700

    finish testing node scaling

--- metagenomescope/tests/assembly_graph/test_assembly_graph.py
+++ metagenomescope/tests/assembly_graph/test_assembly_graph.py
@@ -1,5 +1,6 @@
 from metagenomescope.graph_objects import AssemblyGraph
 from metagenomescope.input_node_utils import negate_node_id
+from metagenomescope import config
 import networkx as nx
 from pytest import approx
 
@@ -16,21 +17,33 @@ def test_scale_nodes():
         "5" : approx(0.4180047),
         "6" : 0
     }
+    nodename2lp = {
+        "1" : config.MID_LONGSIDE_PROPORTION,
+        "2" : config.HIGH_LONGSIDE_PROPORTION,
+        "3" : config.HIGH_LONGSIDE_PROPORTION,
+        "4" : config.MID_LONGSIDE_PROPORTION,
+        "5" : config.MID_LONGSIDE_PROPORTION,
+        "6" : config.LOW_LONGSIDE_PROPORTION
+    }
     seen_nodenames = []
     for node in ag.digraph.nodes:
-        print(nx.get_node_attributes(ag.digraph, "relative_length"))
         name = ag.digraph.nodes[node]["name"]
         rl = ag.digraph.nodes[node]["relative_length"]
+        lp = ag.digraph.nodes[node]["longside_proportion"]
         if name in nodename2rl:
             assert rl == nodename2rl[name]
+            assert lp == nodename2lp[name]
         else:
-            assert rl == nodename2rl[negate_node_id(name)]
+            negated_name = negate_node_id(name)
+            assert rl == nodename2rl[negated_name]
+            assert lp == nodename2lp[negated_name]
         seen_nodenames.append(name)
     assert len(seen_nodenames) == 12
 
 def test_scale_nodes_all_lengths_equal():
-    pass
-    #ag = AssemblyGraph("metagenomescope/tests/input/sample1.gfa")
-    ## This graph has six nodes, with lengths 8, 10, 21, 7, 8, 4.
-    #ag.scale_nodes()
-    #assert ag.digraph.nodes["1"]["relative_length"] = 
+    ag = AssemblyGraph("metagenomescope/tests/input/loop.gfa")
+    # all of the nodes in this graph have length 3
+    ag.scale_nodes()
+    for node in ag.digraph.nodes:
+        assert ag.digraph.nodes[node]["relative_length"] == 0.5
+        assert ag.digraph.nodes[node]["longside_proportion"] == config.MID_LONGSIDE_PROPORTION
