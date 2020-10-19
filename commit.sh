b70a190d3129b50a3fea9f25069578a4377329d1
Marcus Fedarko <mfedarko@umd.edu>
2020-10-18 21:23:47 -0700

    rm unused shell scripts
    minification: not actually that important for now

--- commit.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env bash
-# Assumes the CWD is the root of the MetagenomeScope/ repository.
-bash minify_files.fish
-git add viewer/js/xdot2cy.min.js
-git add viewer/css/viewer_style.min.css
-git add viewer/index.min.html
-git commit
