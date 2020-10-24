26109157087516c1b63e57c5824e4d9e3caebbb2
Marcus Fedarko <mfedarko@umd.edu>
2020-10-24 02:11:14 -0700

    break up asm graph tests into files and rename dir
    much clearer now

--- metagenomescope/tests/layout/test_edge_scaling.py
+++ metagenomescope/tests/layout/test_edge_scaling.py
@@ -1,56 +1,5 @@
 from metagenomescope.graph_objects import AssemblyGraph
-from metagenomescope.input_node_utils import negate_node_id
-from metagenomescope import config
 from pytest import approx
-
-
-def test_scale_nodes():
-    ag = AssemblyGraph("metagenomescope/tests/input/sample1.gfa")
-    # This graph has six nodes, with lengths 8, 10, 21, 7, 8, 4.
-    #                          (for node IDs 1,  2,  3, 4, 5, 6.)
-    ag.scale_nodes()
-    nodename2rl = {
-        "1": approx(0.4180047),
-        "2": approx(0.5525722),
-        "3": 1,
-        "4": approx(0.3374782),
-        "5": approx(0.4180047),
-        "6": 0,
-    }
-    nodename2lp = {
-        "1": config.MID_LONGSIDE_PROPORTION,
-        "2": config.HIGH_LONGSIDE_PROPORTION,
-        "3": config.HIGH_LONGSIDE_PROPORTION,
-        "4": config.MID_LONGSIDE_PROPORTION,
-        "5": config.MID_LONGSIDE_PROPORTION,
-        "6": config.LOW_LONGSIDE_PROPORTION,
-    }
-    seen_nodenames = []
-    for node in ag.digraph.nodes:
-        name = ag.digraph.nodes[node]["name"]
-        rl = ag.digraph.nodes[node]["relative_length"]
-        lp = ag.digraph.nodes[node]["longside_proportion"]
-        if name in nodename2rl:
-            assert rl == nodename2rl[name]
-            assert lp == nodename2lp[name]
-        else:
-            negated_name = negate_node_id(name)
-            assert rl == nodename2rl[negated_name]
-            assert lp == nodename2lp[negated_name]
-        seen_nodenames.append(name)
-    assert len(seen_nodenames) == 12
-
-
-def test_scale_nodes_all_lengths_equal():
-    ag = AssemblyGraph("metagenomescope/tests/input/loop.gfa")
-    # all of the nodes in this graph have length 3
-    ag.scale_nodes()
-    for node in ag.digraph.nodes:
-        assert ag.digraph.nodes[node]["relative_length"] == 0.5
-        assert (
-            ag.digraph.nodes[node]["longside_proportion"]
-            == config.MID_LONGSIDE_PROPORTION
-        )
 
 
 def test_has_edge_weights():
@@ -244,3 +193,4 @@ def test_scale_edges_dup_edges():
     # And it should have the default scaling values
     assert ag.digraph.edges[0, 0]["is_outlier"] == 0
     assert ag.digraph.edges[0, 0]["relative_weight"] == 0.5
+
