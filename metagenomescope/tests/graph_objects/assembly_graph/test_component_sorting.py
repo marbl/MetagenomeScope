a2f45396c0d4e4d8f354a71c1f9f1d85cd596df0
Marcus Fedarko <mfedarko@umd.edu>
2020-11-05 19:33:11 -0800

    TST: beef up component sorting tests re: edge cts
    It looks like things are... really messed up with this.
    Which is nice, since it makes rooting this problem out easier LOL
    
    like what's up with the single edges in cyclic chains in the e. coli
    graph being counted as 372 edges???

--- metagenomescope/tests/graph_objects/assembly_graph/test_component_sorting.py
+++ metagenomescope/tests/graph_objects/assembly_graph/test_component_sorting.py
@@ -15,22 +15,26 @@ def test_component_sorting_simple():
     # Components 1/2 and 3/4 have the same number of nodes/edges/patterns, so
     # the precise ordering is arbitrary. We just check that the number of
     # top-level nodes ("real" nodes or collapsed patterns, stored in the 0th
-    # position of each 2-tuple in wccs) is correct, and that the total number
-    # of "real" nodes in the component (stored in the 1th position of each
-    # 2-tuple) is also correct.
+    # position of each 3-tuple in wccs) is correct, and that the total numbers
+    # of "real" nodes and edges in the component (stored in the 1st and 2nd
+    # position of each 3-tuple) is also correct.
     assert len(wccs) == 4
 
     assert len(wccs[0][0]) == 4
     assert wccs[0][1] == 5
+    assert wccs[0][2] == 4
 
     assert len(wccs[1][0]) == 4
     assert wccs[1][1] == 5
+    assert wccs[1][2] == 4
 
     assert len(wccs[2][0]) == 1
     assert wccs[2][1] == 1
+    assert wccs[2][2] == 0
 
     assert len(wccs[3][0]) == 1
     assert wccs[3][1] == 1
+    assert wccs[3][2] == 0
 
 
 def test_component_sorting_ecoli_graph():
@@ -68,14 +72,16 @@ def test_component_sorting_ecoli_graph():
     # chains with a single node and edge (i.e. they are sorted before the
     # other components with just a single node each). This is because these are
     # more "important" than these other components, at least to us.
-    for wcc_2tuple in wccs[5:11]:
-        wcc = wcc_2tuple[0]
+    for wcc_tuple in wccs[5:11]:
+        wcc = wcc_tuple[0]
         # Check that the component contains just 1 node at the top level (a
         # cyclic chain pattern)...
         assert len(wcc) == 1
         # ... and just 1 "real" node in full (the one node within the cyclic
         # chain)
-        assert wcc_2tuple[1] == 1
+        assert wcc_tuple[1] == 1
+        # ... and just 1 edge
+        assert wcc_tuple[2] == 1
         cyc_id = list(wcc)[0]
         assert ag.is_pattern(cyc_id)
         # This cyclic chain should contain one node and one edge (and no other
