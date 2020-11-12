9a7370de7ba2bf6234d2a7f211557147748fa526
Marcus Fedarko <mfedarko@umd.edu>
2020-11-11 22:10:07 -0800

    Document check_attrs tests

--- metagenomescope/tests/graph_objects/assembly_graph/test_check_attrs.py
+++ metagenomescope/tests/graph_objects/assembly_graph/test_check_attrs.py
@@ -1,3 +1,13 @@
+# These tests check what happens when an input graph has reserved attributes in
+# its nodes / edges. We (currently) store all node/edge data as dicts in a
+# NetworkX graph -- which is fine, but it means that "real" things like node
+# length, edge multiplicity, etc. have to exist alongside "internal" things
+# like node height, edge control point data, etc. To alleviate this problem, we
+# just preemptively disallow input graphs from containing any of these internal
+# fields as attributes. IDEALLY we'd refactor things so that "real" data fields
+# can be named anything, but ... that's gonna be a lot of work and if I
+# refactor this codebase one more time I think my brain is going to leap out of
+# my head and punch me in the face.
 from metagenomescope.graph_objects import AssemblyGraph
 import pytest
 
