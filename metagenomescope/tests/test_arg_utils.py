fc09615e7974d3a5c6cdb913e95878670d9d83f0
Marcus Fedarko <mfedarko@umd.edu>
2020-12-02 12:10:27 -0800

    STY

--- metagenomescope/tests/test_arg_utils.py
+++ metagenomescope/tests/test_arg_utils.py
@@ -23,11 +23,14 @@ import tempfile
 import pytest
 from metagenomescope import arg_utils
 
+
 def test_validate_max_counts():
     # Both bad
     with pytest.raises(ValueError) as e:
         arg_utils.validate_max_counts(-1, -1)
-    assert "Maximum node / maximum edge counts must be at least 1" == str(e.value)
+    assert "Maximum node / maximum edge counts must be at least 1" == str(
+        e.value
+    )
 
     # Just node ct bad
     with pytest.raises(ValueError) as e:
@@ -42,18 +45,22 @@ def test_validate_max_counts():
     # Both good
     arg_utils.validate_max_counts(1, 1)
 
+
 def test_check_dir_existence():
     # Check failure case -- directory path already exists.
     # Based on https://docs.python.org/3/library/tempfile.html#examples
     with tempfile.TemporaryDirectory() as tmpdir:
         with pytest.raises(FileExistsError) as e:
             arg_utils.check_dir_existence(tmpdir)
-        assert "Output directory {} already exists.".format(tmpdir) == str(e.value)
+        assert "Output directory {} already exists.".format(tmpdir) == str(
+            e.value
+        )
 
     # Check success case -- path does not already exist.
     # If this test breaks for you then uhhhh I guess submit a PR and ask me to
     # rewrite this test lol, sorry
     arg_utils.check_dir_existence("./this-should-not-exist-if-it-does-oh-no/")
+
 
 def test_create_output_dir():
     # Test creating multiple dirs at once using makedirs
