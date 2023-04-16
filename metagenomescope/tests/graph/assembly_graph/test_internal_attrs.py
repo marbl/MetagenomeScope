149d79c88a440fc00e008d78ea5e191fd8a93ba9
Marcus Fedarko <mfedarko@ucsd.edu>
2023-04-15 22:17:30 -0700

    TST: adjust check_attrs test
    now that function no longer exists, but still nice to check that
    "reserved" node attrs are now OK

--- metagenomescope/tests/graph/assembly_graph/test_internal_attrs.py
+++ metagenomescope/tests/graph/assembly_graph/test_internal_attrs.py
@@ -13,12 +13,10 @@ import pytest
 
 
 def test_check_attrs_node():
-    with pytest.raises(ValueError) as einfo:
-        AssemblyGraph("metagenomescope/tests/input/check_attrs_test_node.gml")
-    assert "has reserved attribute(s) {'height'}." in str(einfo.value)
+    # Shouldn't fail with an error
+    AssemblyGraph("metagenomescope/tests/input/check_attrs_test_node.gml")
 
 
 def test_check_attrs_edge():
-    with pytest.raises(ValueError) as einfo:
-        AssemblyGraph("metagenomescope/tests/input/check_attrs_test_edge.gml")
-    assert "has reserved attribute(s) {'ctrl_pt_coords'}." in str(einfo.value)
+    # Shouldn't fail with an error
+    AssemblyGraph("metagenomescope/tests/input/check_attrs_test_edge.gml")
