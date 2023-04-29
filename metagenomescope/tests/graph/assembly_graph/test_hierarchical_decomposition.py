bc71b3e7449374a4c831a3fa9718b67c98ac4f0b
Marcus Fedarko <mfedarko@ucsd.edu>
2023-04-28 17:26:23 -0700

    TST: updating h-decomp tests re chain splitting
    hmmm why is the first test failing? with a weird error at that.
    digging in to it

--- metagenomescope/tests/graph/assembly_graph/test_hierarchical_decomposition.py
+++ metagenomescope/tests/graph/assembly_graph/test_hierarchical_decomposition.py
@@ -70,11 +70,11 @@ def test_bubble_with_1_in_node():
     level as the parent of 1R, so we should merge 1L back into 1R.
     """
     ag = AssemblyGraph("metagenomescope/tests/input/bubble_test_1_in.gml")
+    # TODO: when we remove "unnecessary" split nodes, node 1 should be merged
+    # back.
     assert len(ag.decomposed_graph.nodes) == 1
     assert len(ag.decomposed_graph.edges) == 0
-    # TODO: uncomment when chain merging and unneeded split node merging are
-    # added in
-    # assert len(ag.chains) == 1
+    assert len(ag.chains) == 1
     assert len(ag.cyclic_chains) == 0
     assert len(ag.frayed_ropes) == 0
     assert len(ag.bubbles) == 1
@@ -89,70 +89,27 @@ def test_bubble_chain_identification():
           \ / \ /
            3   6
 
-    First, we should collapse one of the bubbles (order shouldn't impact
-    result). Let's say the leftmost bubble is collapsed first.
+    This should be collapsed into a chain that looks like...
 
-        +-------+
-        |   2   | 5
-        |  / \  |/ \
-    0 ->| 1   4 |   7 -> 8
-        |  \ /  |\ /
-        |   3   | 6
-        +-------+
-
-    Then a valid bubble would exist where the first bubble is the start node:
-
-        +------+
-        |   5  |
-        |  / \ |
-    0 ->|B1   7|-> 8
-        |  \ / |
-        |   6  |
-        +------+
-
-    However, this is silly, because nothing about the input graph implied any
-    sort of hierarchy between these two bubbles. The code should detect that a
-    bubble is being used as the start node of this other bubble (or as the end
-    node, if the rightmost bubble was detected first), and then handle this
-    by duplicating just the shared node between the bubbles:
-
-        +-------+-------+
-        |   2   |   5   |
-        |  / \  |  / \  |
-    0 ->| 1   4 = 4   7 | -> 8
-        |  \ /  |  \ /  |
-        |   3   |   6   |
-        +-------+-------+
-
-
-    0 -> (Bubble 1) = (Bubble 2) -> 8
-
-
-        +-------+-------+
-        |   2   |   5   |
-        |  / \  |  / \  |
-    0 ->| 1   4 = 4   7 | -> 8
-        |  \ /  |  \ /  |
-        |   3   |   6   |
-        +-------+-------+
-
-    ... which makes sense.
-
-    Also at some point we should merge everything into a chain going from 0 to
-    8. Ideally, chains will support _merging_: so when a chain is created that
-    includes another chain at the top-level, that chain will be subsumed into
-    the overall chain. So even if 0 -> 1 or 7 -> 8 is created first, the
-    resulting graph should be the same.
+    +----------------------------+
+    |     +-------+-------+      |
+    |     |   2   |   5   |      |
+    |     |  / \  |  / \         |
+    | 0 ->| 1   4 = 4   7 | -> 8 |
+    |     |  \ /  |  \ /  |      |
+    |     |   3   |   6   |      |
+    |     +-------+-------+      |
+    +----------------------------+
     """
     ag = AssemblyGraph("metagenomescope/tests/input/bubble_chain_test.gml")
     assert len(ag.decomposed_graph.nodes) == 1
     assert len(ag.decomposed_graph.edges) == 0
-    # TODO: Fix, as described above (everything should be in 1 chain, which
-    # contains the two bubbles)
-    # assert len(ag.chains) == 1
+    assert len(ag.chains) == 1
     assert len(ag.cyclic_chains) == 0
     assert len(ag.frayed_ropes) == 0
     assert len(ag.bubbles) == 2
+    # TODO: when we remove "unnecessary" split nodes, the splits of nodes 1 and
+    # 7 should be merged back together. Right now, they are still split up.
 
 
 def test_bubble_cyclic_chain_identification():
