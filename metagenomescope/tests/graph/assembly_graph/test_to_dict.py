f60527b6f26df1158b2cf766d2176c98676735d5
Marcus Fedarko <mfedarko@ucsd.edu>
2023-05-12 21:10:42 -0700

    TST: call layout() in to_dict() test
    still failing, but it'll stop failing when we fix layout. or uh,
    at least when we fix layout and then to_dict.

--- metagenomescope/tests/graph/assembly_graph/test_to_dict.py
+++ metagenomescope/tests/graph/assembly_graph/test_to_dict.py
@@ -3,6 +3,7 @@ from metagenomescope.graph import AssemblyGraph
 
 def test_to_dict_simple():
     ag = AssemblyGraph("metagenomescope/tests/input/sample1.gfa")
-
+    ag.layout()
     data = ag.to_dict()
     assert type(data) == dict
+    # TODO: for the love of everything please expand this test
