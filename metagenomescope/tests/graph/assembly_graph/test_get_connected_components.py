a7f589366c9d840adde7b5035f8af4856de37e59
Marcus Fedarko <mfedarko@ucsd.edu>
2023-05-12 20:31:02 -0700

    TST: fix cc sorting tests re: refactor

--- metagenomescope/tests/graph/assembly_graph/test_get_connected_components.py
+++ metagenomescope/tests/graph/assembly_graph/test_get_connected_components.py
@@ -7,7 +7,7 @@ def test_component_sorting_simple():
     wccs = ag.get_connected_components()
     # Four ccs:
     # 1. 1+, 2+, 3+, Chain of [4-, 5+] (the chain is counted as a single node
-    #                                   in the decomposed digraph)
+    #                                   in the decomposed graph)
     # 2. 1-, 2-, 3-, Chain of [5-, 4+]
     # 3. 6+
     # 4. 6-
@@ -44,20 +44,25 @@ def test_component_sorting_ecoli_graph():
     seen_89_in_largest_cc = False
     for node_id in wccs[0][0]:
         if not ag.is_pattern(node_id):
-            if ag.decomposed_graph.nodes[node_id]["name"] == "89":
+            if ag.nodeid2obj[node_id].name == "89":
                 seen_89_in_largest_cc = True
                 break
         else:
-            # Recursively go through all patterns until we find 89
+            # Recursively go through pattern to check for node 89
+            # Why did I write this test in this way, and not just check for a
+            # node that we know will always be in the top level? Mmmm I have a
+            # really good answer for that, but I leave it as an exercise to the
+            # reader so yeah sorry can't explain it here uh bye my bad
             pattern_queue = [node_id]
             while len(pattern_queue) > 0:
-                pattern = pattern_queue.pop()
-                patt_obj = ag.id2pattern[pattern]
-                for child_node_id in patt_obj.node_ids:
+                patt_id = pattern_queue.pop()
+                patt_obj = ag.pattid2obj[patt_id]
+                for child_node in patt_obj.nodes:
+                    child_node_id = child_node.unique_id
                     if ag.is_pattern(child_node_id):
                         pattern_queue.append(child_node_id)
                     else:
-                        if ag.graph.nodes[child_node_id]["name"] == "89":
+                        if ag.nodeid2obj[child_node_id].name == "89":
                             seen_89_in_largest_cc = True
                             break
                 if seen_89_in_largest_cc:
@@ -65,10 +70,12 @@ def test_component_sorting_ecoli_graph():
 
     assert seen_89_in_largest_cc
 
-    # Assert that components in the 1-indexed range [6, 11] are all cyclic
-    # chains with a single node and edge (i.e. they are sorted before the
+    # Assert that components in the 1-indexed range [6, 11] are all components
+    # of a single node and edge (i.e. they are sorted before the
     # other components with just a single node each). This is because these are
-    # more "important" than these other components, at least to us.
+    # more "important" than these other components, at least to us. (We used to
+    # identify nodes like this as "cyclic chains," but we don't any more.
+    # Alas. But they should still be sorted highly due to having an edge!)
     for wcc_tuple in wccs[5:11]:
         wcc = wcc_tuple[0]
         # Check that the component contains just 1 node at the top level (a
@@ -80,10 +87,6 @@ def test_component_sorting_ecoli_graph():
         # ... and just 1 edge
         assert wcc_tuple[2] == 1
         cyc_id = list(wcc)[0]
-        assert ag.is_pattern(cyc_id)
-        # This cyclic chain should contain one node and one edge (and no other
-        # patterns within itself).
-        assert ag.id2pattern[cyc_id].get_counts(ag) == [1, 1, 0]
 
 
 # TODO: Add more comprehensive tests that things like number of nodes within
