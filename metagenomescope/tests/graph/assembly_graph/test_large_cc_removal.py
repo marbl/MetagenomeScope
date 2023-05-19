c48928e23143fe4a6956e6af433d36b3cd48e042
Marcus Fedarko <mfedarko@ucsd.edu>
2023-05-18 22:27:11 -0700

    TST: rename & fix cc max node/edge ct tests
    didn't really make sense to have these under "layout" tests, at least
    not anymore. when we fix layout like within the next week (knocked on
    wood irl at the desk i am sitting at just now), we can add actual
    tests for that

--- metagenomescope/tests/graph/assembly_graph/test_large_cc_removal.py
+++ metagenomescope/tests/graph/assembly_graph/test_large_cc_removal.py
@@ -7,14 +7,7 @@ def test_ccs_avoided_due_to_max_node_ct(capsys):
     # For now, just check the print messages.
     # https://docs.pytest.org/en/stable/capture.html
     captured = capsys.readouterr()
-    # (the first message about not laying out a cmp with 5 nodes and 4 edges
-    # occurs twice)
-    exp_msgs = (
-        "Ignoring a component with 5 nodes and 4 edges.",
-        "Laying out small (each containing < 5 nodes) remaining component(s)...",
-    )
-    for m in exp_msgs:
-        assert m in captured.out
+    assert "Ignoring a component with 5 nodes and 4 edges." in captured.out
 
 
 def test_error_if_all_ccs_avoided():
