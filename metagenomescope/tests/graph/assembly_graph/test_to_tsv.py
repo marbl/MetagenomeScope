208d57f45cdcda54808299f7dbea93e1cccd7127
Marcus Fedarko <mfedarko@ucsd.edu>
2023-05-18 22:20:51 -0700

    TST: to_tsv() with remaining splits/fakes

--- metagenomescope/tests/graph/assembly_graph/test_to_tsv.py
+++ metagenomescope/tests/graph/assembly_graph/test_to_tsv.py
@@ -48,3 +48,55 @@ def test_to_tsv_bt1():
     finally:
         os.close(fh)
         os.unlink(fn)
+
+
+def test_to_tsv_bubble_cyclic_chain():
+    r"""The input graph consists of bubbles in a cyclic chain. Decomposes into:
+
+    +=======================================+
+    |                                       |
+    | +-------+-------+--------+---------+  |
+    | |   2   |   5   |   8    |    11   |  |
+    | |  / \  |  / \  |  / \   |   /  \  |  |
+    +== 1   4 = 4   7 = 7   10 = 10    1 ===+
+      |  \ /  |  \ /  |  \ /   |   \  /  |
+      |   3   |   6   |   9    |    12   |
+      +-------+-------+--------+---------+
+
+    The decomposition has fake nodes and edges, so this checks how to_tsv()
+    handles those.
+    """
+    ag = AssemblyGraph(
+        "metagenomescope/tests/input/bubble_cyclic_chain_test.gml"
+    )
+    # double-check that the decomposition worked
+    assert len(ag.decomposed_graph.nodes) == 1
+    assert len(ag.decomposed_graph.edges) == 0
+    assert len(ag.chains) == 0
+    assert len(ag.cyclic_chains) == 1
+    assert len(ag.frayed_ropes) == 0
+    assert len(ag.bubbles) == 4
+    assert len(ag.graph.nodes) == 16
+    assert len(ag.graph.edges) == 20
+
+    try:
+        fh, fn = tempfile.mkstemp(suffix=".tsv")
+        ag.to_tsv(fn)
+        df = pd.read_csv(fn, sep="\t", index_col=0)
+        pd.testing.assert_frame_equal(
+            df,
+            pd.DataFrame(
+                {
+                    "Nodes": [16],
+                    "Edges": [20],
+                    "Bubbles": [4],
+                    "Chains": [0],
+                    "CyclicChains": [1],
+                    "FrayedRopes": [0],
+                },
+                index=pd.Index([1], name="ComponentID"),
+            ),
+        )
+    finally:
+        os.close(fh)
+        os.unlink(fn)
