a2ee1d15bc1b43dc521437ec431909fda7f9cd62
Marcus Fedarko <mfedarko@ucsd.edu>
2025-10-28 23:59:51 -0400

    move styling from CSS to Python; expand cy
    spent SO LONG messing with trying to prevent the canvas from
    jumping to the left when we resize the cy div. it is infuriating
    how it works so elegantly at
    https://js.cytoscape.org/demos/fcose-gene/ but here we have
    the janky jumps.
    
    i have a few more ideas but eventually it will be time to either
    right-align the control panel like they do (fixes the issue)
    or just cut our losses and move on to things that actually matter

--- metagenomescope/main.py
+++ metagenomescope/main.py
@@ -54,7 +54,8 @@ def run(
     for e in ag.graph.edges:
         edges.append({"data": {"source": str(e[0]), "target": str(e[1])}})
 
-    app = dash.Dash(__name__, title="MgSc")
+    # update_title=None prevents Dash's default "Updating..." page title change
+    app = dash.Dash(__name__, title="MgSc", update_title=None)
     CONTROLS_TOGGLER_ICON_CLASSES = "glyphicon glyphicon-menu-hamburger"
     app.layout = html.Div(
         [
@@ -99,10 +100,13 @@ def run(
                 id="controls",
                 style={
                     "position": "absolute",
-                    "width": "20em",
                     "top": "0em",
                     "bottom": "0em",
+                    "left": "0em",
+                    "width": "20em",
                     "text-align": "center",
+                    # only show the scrollbar when needed - firefox doesn't
+                    # seem to need this, but chrome does
                     "overflow-y": "auto",
                     "z-index": "1",
                     "background-color": "#123",
@@ -114,6 +118,28 @@ def run(
             # cy
             html.Div(
                 id="cyDiv",
+                style={
+                    "position": "absolute",
+                    "left": "20em",
+                    "top": "0em",
+                    "bottom": "0em",
+                    "right": "0em",
+                    "z-index": "0",
+                    # for debugging layout jank...
+                    # "border": "3px solid #00ff00",
+                    # NOTE: so, if you adjust this
+                    # (like https://js.cytoscape.org/demos/fcose-gene/) so that
+                    # the control panel is on the right and we're changing the
+                    # "right" style property of #cyDiv, then the canvas
+                    # contents no longer shift (while still resizing the
+                    # canvas to fill the void left by the hidden control
+                    # panel). I spent multiple hours trying to find a way
+                    # to make this work on the left side of the canvas and was
+                    # unsuccessful -- I think it might be something to do with
+                    # how HTML canvases work under the hood. idk. anyway this
+                    # at least adds a smooth transition.
+                    "transition": "left 0.2s",
+                },
             ),
         ],
     )
@@ -121,12 +147,14 @@ def run(
     @callback(
         Output("controls", "className"),
         Output("controlsTogglerIcon", "className"),
+        Output("cyDiv", "style"),
         State("controls", "className"),
+        State("cyDiv", "style"),
         Input("controlsToggler", "n_clicks"),
         prevent_initial_call=True,
     )
-    def toggle_controls(controls_classes, n_clicks):
-        """Toggles visibility of the controls div.
+    def toggle_controls(controls_classes, cy_div_style, n_clicks):
+        """Toggles visibility of the control panel's div.
 
         Also toggles the color of the toggler hamburger icon -- it is colored
         light when the controls are visible, and dark when the controls are not
@@ -134,11 +162,16 @@ def run(
         can control the background color of the graph, but it's ok for now.)
         """
         if "notviewable" in controls_classes:
-            return ("", CONTROLS_TOGGLER_ICON_CLASSES)
+            # Make the control panel visible again
+            cy_div_style["left"] = "20em"
+            return ("", CONTROLS_TOGGLER_ICON_CLASSES, cy_div_style)
         else:
+            # Hide the control panel
+            cy_div_style["left"] = "0em"
             return (
                 "notviewable",
                 CONTROLS_TOGGLER_ICON_CLASSES + " darkToggler",
+                cy_div_style,
             )
 
     @callback(
@@ -152,14 +185,9 @@ def run(
             elements=nodes + edges,
             layout={"name": "circle"},
             style={
-                "position": "absolute",
-                "left": "20em",
-                "right": "0em",
-                "top": "0em",
-                "bottom": "0em",
-                "border": "1px solid black",
+                "width": "100%",
+                "height": "100%",
             },
-            responsive=True,
         )
 
     app.run(debug=True)
