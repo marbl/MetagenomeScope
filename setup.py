22c06196ebf9ab5a10cb76b76d31f207f6ef4522
Marcus Fedarko <mfedarko@ucsd.edu>
2025-11-13 22:49:57 -0500

    include pandas for testing to_tsv()
    eh idk if we will even keep to_tsv around for anything

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ setup(
     # version changes how the ** operator is formatted (no surrounding spaces,
     # usually). Not being consistent causes the build to fail, hence the pin.
     extras_require={
-        "dev": ["pytest", "pytest-cov", "flake8", "black>=22.1.0"]
+        "dev": ["pytest", "pytest-cov", "flake8", "black>=22.1.0", "pandas"]
     },
     entry_points={"console_scripts": ["mgsc=metagenomescope._cli:run_script"]},
     # Based on dash-cytoscape's min python version.
