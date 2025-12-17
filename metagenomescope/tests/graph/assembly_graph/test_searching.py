5b3d3ff95f3b5bdf528b59e90f2243b05e36a157
Marcus Fedarko <mfedarko@ucsd.edu>
2025-12-16 19:11:06 -0500

    test missing node names in search

--- metagenomescope/tests/graph/assembly_graph/test_searching.py
+++ metagenomescope/tests/graph/assembly_graph/test_searching.py
@@ -1,4 +1,6 @@
+import pytest
 from metagenomescope.graph import AssemblyGraph
+from metagenomescope.errors import UIError
 
 
 def test_get_nodename2ccnum_singlenode():
@@ -20,3 +22,20 @@ def test_get_nodename2ccnum_multinode_and_input_duplicates():
     ag = AssemblyGraph("metagenomescope/tests/input/flye_yeast.gv")
     d = ag.get_nodename2ccnum("1, 35, 48,,,1,35,,48")
     assert d == {"1": 1, "35": 1, "48": 2}
+
+
+def test_get_nodename2ccnum_missing_node():
+    ag = AssemblyGraph("metagenomescope/tests/input/flye_yeast.gv")
+    with pytest.raises(UIError) as ei:
+        ag.get_nodename2ccnum("1, 35, 48,,,1,35,,48,poop")
+    assert str(ei.value) == 'Can\'t find a node with name "poop" in the graph.'
+
+
+def test_get_nodename2ccnum_missing_multiple_nodes():
+    ag = AssemblyGraph("metagenomescope/tests/input/flye_yeast.gv")
+    with pytest.raises(UIError) as ei:
+        ag.get_nodename2ccnum("1, 35, 48,,,1,35,,48,poop,help,what")
+    # the list of bad node names is sorted so we know it should look like this
+    assert str(ei.value) == (
+        'Can\'t find nodes with names "help", "poop", "what" in the graph.'
+    )
