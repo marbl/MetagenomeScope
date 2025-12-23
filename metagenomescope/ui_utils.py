2e932d6ef17f59e227005b0635a3623ca5663e37
Marcus Fedarko <mfedarko@ucsd.edu>
2025-12-23 18:49:50 -0500

    "Component #" -> "CC#" in search err/warn tbl
    looks better when there are lots of nodes in the table

--- metagenomescope/ui_utils.py
+++ metagenomescope/ui_utils.py
@@ -570,7 +570,7 @@ def summarize_undrawn_nodes(undrawn_nodes, nn2ccnum, num_searched_for_nodes):
         intro = html.Div(f"{s1} {s2}")
 
         theader = [
-            html.Thead(html.Tr([html.Th("Component #"), html.Th("Nodes")]))
+            html.Thead(html.Tr([html.Th("CC#"), html.Th("Nodes")]))
         ]
         rows = []
         for c in sorted(undrawn_cc_to_nodes):
