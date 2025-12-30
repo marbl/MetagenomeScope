50708626caf060b4de60573d1275f4a0a644dfba
Marcus Fedarko <mfedarko@ucsd.edu>
2025-12-29 23:33:29 -0500

    integration test for #272

--- metagenomescope/tests/graph/assembly_graph/test_init.py
+++ metagenomescope/tests/graph/assembly_graph/test_init.py
@@ -1,4 +1,6 @@
+import pytest
 from metagenomescope.graph import AssemblyGraph
+from metagenomescope.errors import GraphParsingError
 
 
 def test_flye_yeast_lengths_are_ints():
@@ -7,3 +9,13 @@ def test_flye_yeast_lengths_are_ints():
     ag = AssemblyGraph("metagenomescope/tests/input/flye_yeast.gv")
     assert ag.total_seq_len == 23175100
     assert type(ag.total_seq_len) is int
+
+
+def test_sanity_checking_already_splitsuffix():
+    # https://github.com/marbl/MetagenomeScope/issues/272
+    with pytest.raises(GraphParsingError) as ei:
+        AssemblyGraph("metagenomescope/tests/input/splitsuffixtest.gml")
+    assert str(ei.value) == (
+        'A node named "contig-100_3-L" exists in the graph. Nodes cannot '
+        'have names that end in "-L" or "-R".'
+    )
