93ba048c4d30effa7ed4689ee619aa01a1d8b702
Marcus Fedarko <mfedarko@ucsd.edu>
2026-01-08 19:51:03 -0500

    Update draw() comment

--- metagenomescope/main.py
+++ metagenomescope/main.py
@@ -1750,12 +1750,11 @@ def run(
         prevent_initial_call=True,
     )
     def draw(curr_done_flushing):
-        # as far as I can tell, this gets triggered whenever doneFlushing is
-        # updated -- even if it is updated to the exact same thing as it was
-        # before. To avoid making us redraw the entire graph if the user just
-        # like specified a node name that didn't exist when searching for a cc,
-        # we use the "requestGood" key in doneFlushing to let us know when we
-        # ACTUALLY want to redraw the graph.
+        # this gets triggered whenever doneFlushing is updated -- even if it is
+        # just {"requestGood": False}. So, to avoid making us redraw the entire
+        # graph if the user just like specified a node name that didn't exist
+        # when searching for a cc, we check the "requestGood" key in
+        # doneFlushing to let us know when to actually draw the graph.
         if curr_done_flushing["requestGood"]:
             logging.debug(
                 "Request good, so flushing should be done. Creating JSON "
