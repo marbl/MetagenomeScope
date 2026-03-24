d970981c9d5b17033a10117942d0e8476a1367e4
Marcus Fedarko <mfedarko@ucsd.edu>
2026-03-23 21:28:58 -0400

    more #67 testing

---
+++ metagenomescope/tests/graph/assembly_graph/test_nonredundant.py
@@ -0,0 +1,68 @@
+import pytest
+from metagenomescope.errors import WeirdError
+from metagenomescope.graph import AssemblyGraph
+
+
+def _get_user_edge_ids(cc):
+    eids = set()
+    for e in cc.edges:
+        if not e.is_fake:
+            eids.add(e.get_userspecified_id())
+    return eids
+
+
+def _get_node_basenames(cc):
+    # using a set takes care of split nodes nicely
+    bns = set()
+    for n in cc.nodes:
+        bns.add(n.basename)
+    return bns
+
+
+def test_flye_yeast_nr_ccs():
+    ag = AssemblyGraph("metagenomescope/tests/input/flye_yeast.gv")
+
+    # make sure these exact ccs are reported as the nonredundant ones
+    assert ag.get_nr_cc_nums() == {1, 2, 5, 6, 7, 9}
+
+    # ... and that they correspond to the actual components we expect
+    assert len(ag.get_cc_by_num(1).nodes) == 39
+    assert len(ag.get_cc_by_num(2).nodes) == 10
+    assert len(ag.get_cc_by_num(5).nodes) == 2
+    assert len(ag.get_cc_by_num(6).nodes) == 2
+    assert len(ag.get_cc_by_num(7).nodes) == 1
+    assert len(ag.get_cc_by_num(9).nodes) == 1
+
+    assert _get_user_edge_ids(ag.get_cc_by_num(5)) == {"24"}
+    assert _get_user_edge_ids(ag.get_cc_by_num(6)) == {"39"}
+    assert _get_user_edge_ids(ag.get_cc_by_num(7)) == {"2"}
+    assert _get_user_edge_ids(ag.get_cc_by_num(9)) == {"21"}
+
+
+def test_sample1_nr_ccs():
+    ag = AssemblyGraph("metagenomescope/tests/input/sample1.gfa")
+
+    assert ag.get_nr_cc_nums() == {1, 3}
+    assert _get_node_basenames(ag.get_cc_by_num(1)) == {
+        "1",
+        "2",
+        "3",
+        "-4",
+        "5",
+    }
+    assert _get_node_basenames(ag.get_cc_by_num(3)) == {"6"}
+
+
+def test_gml_nr_ccs():
+    ag = AssemblyGraph("metagenomescope/tests/input/aug1_subgraph.gml")
+    with pytest.raises(WeirdError) as ei:
+        ag.get_nr_cc_nums()
+    assert str(ei.value) == ( 
+        "Nonredundant component information not set. Either you " 
+        "called this too early OR this graph does not have " 
+        "reverse-complementary versions of things." 
+    ) 
+
+def test_flye_yeast_nr_ccs_extra_edge():
+    ag = AssemblyGraph("metagenomescope/tests/input/flye_yeast_extra_edge_in_twin.gv")
+    assert ag.get_nr_cc_nums() == {1, 2, 3, 5, 6, 7, 9}
