4d5a51240fa5421ae8cbe1005444447a08229662
Marcus Fedarko <mfedarko@ucsd.edu>
2026-05-19 21:03:13 -0400

    slightly lighter blue for darkened-node outline

--- metagenomescope/cy_config.py
+++ metagenomescope/cy_config.py
@@ -255,5 +255,5 @@ SELECTED_OBJ_OUTLINE_COLOR = "#dadaf7"
 
 # if we are indicating selected nodes by darkening them, then their text will
 # now be a light color by default. Thus, use a dark blue border for them by
-# default, to mirror SELECTED_OBJ_OUTLINE_COLOR
-SELECTED_OBJ_DARK_OUTLINE_COLOR = "#000055"
+# default, to imitate SELECTED_OBJ_OUTLINE_COLOR
+SELECTED_OBJ_DARK_OUTLINE_COLOR = "#000077"
